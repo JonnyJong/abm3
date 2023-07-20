c7723538322f5b0ae125ba593d650da01f65ebc0
JonnyJong <jonnyjong@outlook.com>
2023-07-20 20:47:16 +0800

    fix: setting expand

--- ts/ui/settings.ts
+++ ts/ui/settings.ts
@@ -14,6 +14,8 @@ export class SettingItem {
   body: HTMLDivElement;
   onConfirm: Function | undefined;
   onReset: Function | undefined;
+  private _confrimBtn: any;
+  private _resetBtn: any;
   constructor(option: SettingItemOption) {
     this.element = document.createElement('div');
     this.element.classList.add('setting-item');
@@ -21,19 +23,21 @@ export class SettingItem {
       this.element.classList.add(...option.classList);
     }
     this.element.innerHTML = layout('ui/setting-item', option);
+    this._confrimBtn = this.element.querySelector('.setting-item-confirm');
+    this._resetBtn = this.element.querySelector('.setting-item-reset');
     this.head = (this.element.querySelector('.setting-item-head') as HTMLDivElement);
     this.body = (this.element.querySelector('.setting-item-body') as HTMLDivElement);
     this.element.addEventListener('click',(ev)=>{
       let path = ev.composedPath();
-      if (path.includes(this.head)) return;
+      if (path.includes(this.head) || path.includes(this._confrimBtn) || path.includes(this._resetBtn)) return;
       this.element.classList.toggle('setting-item-expanded');
-this.element.style.setProperty('--height', this.body.getBoundingClientRect().height + 'px');
+      this.element.style.setProperty('--height', this.body.getBoundingClientRect().height + 'px');
     });
-    this.element.querySelector('.setting-item-confirm')?.addEventListener('click',()=>{
+    this._confrimBtn?.addEventListener('click',()=>{
       if (typeof this.onConfirm !== 'function') return;
       this.onConfirm();
     });
-    this.element.querySelector('.setting-item-reset')?.addEventListener('click',()=>{
+    this._resetBtn?.addEventListener('click',()=>{
       if (typeof this.onReset !== 'function') return;
       this.onReset();
     });
