127a8de7924feb8ddc31e57563c4f2c2dd08c786
JonnyJong <jonnyjong@outlook.com>
2023-07-30 11:27:44 +0800

    ui setting-item

--- ts/ui/settings.ts
+++ ts/ui/settings.ts
@@ -1,45 +1,91 @@
 import { layout } from "../helper/layout";
 
-type SettingItemOption = {
-  icon?: string,
-  name: string,
-  description?: string,
-  classList?: string[],
-  confirm?: boolean,
-  reset?: boolean,
-};
-export class SettingItem {
-  element: HTMLDivElement;
-  head: HTMLDivElement;
-  body: HTMLDivElement;
-  onConfirm: Function | undefined;
-  onReset: Function | undefined;
-  private _confrimBtn: any;
-  private _resetBtn: any;
-  constructor(option: SettingItemOption) {
-    this.element = document.createElement('div');
-    this.element.classList.add('setting-item');
-    if (Array.isArray(option.classList)) {
-      this.element.classList.add(...option.classList);
-    }
-    this.element.innerHTML = layout('ui/setting-item', { option });
-    this._confrimBtn = this.element.querySelector('.setting-item-confirm');
-    this._resetBtn = this.element.querySelector('.setting-item-reset');
-    this.head = (this.element.querySelector('.setting-item-head') as HTMLDivElement);
-    this.body = (this.element.querySelector('.setting-item-body') as HTMLDivElement);
-    this.element.addEventListener('click',(ev)=>{
+export class UISettingItem extends HTMLElement{
+  private _inited: boolean = false;
+  private _icon!: HTMLDivElement;
+  private _name!: HTMLDivElement;
+  private _desc!: HTMLDivElement;
+  private _head!: HTMLDivElement;
+  private _headShell!: HTMLDivElement;
+  private _body!: HTMLDivElement;
+  connectedCallback() {
+    if (this._inited) return;
+    this._inited = true;
+    this.innerHTML = layout('ui/setting-item', {body: true});
+    this._icon = (this.querySelector('.ui-setting-item-icon') as HTMLDivElement);
+    this._name = (this.querySelector('.ui-setting-item-name') as HTMLDivElement);
+    this._desc = (this.querySelector('.ui-setting-item-desc') as HTMLDivElement);
+    this._head = (this.querySelector('.ui-setting-item-head') as HTMLDivElement);
+    this._headShell = (this.querySelector('.ui-setting-item-head-shell') as HTMLDivElement);
+    this._body = (this.querySelector('.ui-setting-item-body') as HTMLDivElement);
+    this._headShell.addEventListener('click',(ev)=>{
       let path = ev.composedPath();
-      if (path.includes(this.head) || path.includes(this._confrimBtn) || path.includes(this._resetBtn)) return;
-      this.element.classList.toggle('setting-item-expanded');
-      this.element.style.setProperty('--height', this.body.getBoundingClientRect().height + 'px');
+      if (path.includes(this._head)) return;
+      this.classList.toggle('ui-setting-item-expanded');
+      this.style.setProperty('--height', this._body.getBoundingClientRect().height + 'px');
     });
-    this._confrimBtn?.addEventListener('click',()=>{
-      if (typeof this.onConfirm !== 'function') return;
-      this.onConfirm();
-    });
-    this._resetBtn?.addEventListener('click',()=>{
-      if (typeof this.onReset !== 'function') return;
-      this.onReset();
-    });
-  };
-};
+  }
+  set icon(value: string | undefined){
+    if (typeof value === 'string' && value !== '') {
+      this._icon.innerHTML = `<div class="icon icon-${value}"></div>`;
+    } else {
+      this._icon.innerHTML = '';
+    }
+  }
+  get name(): string {
+    return this._name.innerHTML;
+  }
+  set name(value: string) {
+    this._name.innerHTML = String(value);
+  }
+  get description(): string {
+    return this._desc.innerHTML;
+  }
+  set description(value: string) {
+    this._desc.innerHTML = String(value);
+  }
+  get head() {
+    return this._head;
+  }
+  get body() {
+    return this._body;
+  }
+}
+export class UISettingItemChild extends HTMLElement{
+  private _inited: boolean = false;
+  private _icon!: HTMLDivElement;
+  private _name!: HTMLDivElement;
+  private _desc!: HTMLDivElement;
+  private _head!: HTMLDivElement;
+  connectedCallback() {
+    if (this._inited) return;
+    this._inited = true;
+    this.innerHTML = layout('ui/setting-item', {body: false});
+    this._icon = (this.querySelector('.ui-setting-item-icon') as HTMLDivElement);
+    this._name = (this.querySelector('.ui-setting-item-name') as HTMLDivElement);
+    this._desc = (this.querySelector('.ui-setting-item-desc') as HTMLDivElement);
+    this._head = (this.querySelector('.ui-setting-item-head') as HTMLDivElement);
+  }
+  set icon(value: string | undefined){
+    if (typeof value === 'string' && value !== '') {
+      this._icon.innerHTML = `<div class="icon icon-${value}"></div>`;
+    } else {
+      this._icon.innerHTML = '';
+    }
+  }
+  get name(): string {
+    return this._name.innerHTML;
+  }
+  set name(value: string) {
+    this._name.innerHTML = String(value);
+  }
+  get description(): string {
+    return this._desc.innerHTML;
+  }
+  set description(value: string) {
+    this._desc.innerHTML = String(value);
+  }
+  get head() {
+    return this._head;
+  }
+}
