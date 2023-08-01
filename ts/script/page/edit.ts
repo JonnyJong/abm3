6350fb1074d839f311d561a8e77b1b9a00d9f2d8
JonnyJong <jonnyjong@outlook.com>
2023-08-01 20:24:32 +0800

    change: page edit(undone)

--- ts/script/page/edit.ts
+++ ts/script/page/edit.ts
@@ -10,6 +10,7 @@ function getEditTemplate(): SettingOption[] {
     {
       type: 'item',
       name: '<ui-lang>edit.title</ui-lang>',
+      icon: 'FullAlpha',
       body: [],
       key: 'title',
       head: [
@@ -17,12 +18,14 @@ function getEditTemplate(): SettingOption[] {
           type: 'input',
           key: 'value',
           data: 'title',
+          classList: ['edit-title'],
         }
       ],
     },
     {
       type: 'item',
       name: '<ui-lang>edit.tags</ui-lang>',
+      icon: 'Tag',
       head: [],
       key: 'tags',
       body: [
@@ -37,6 +40,7 @@ function getEditTemplate(): SettingOption[] {
     {
       type: 'item',
       name: '<ui-lang>edit.categories</ui-lang>',
+      icon: 'Folder',
       head: [],
       key: 'cateories',
       body: [
@@ -51,6 +55,7 @@ function getEditTemplate(): SettingOption[] {
     {
       type: 'item',
       name: '<ui-lang>edit.markdown</ui-lang>',
+      icon: 'AlignLeft',
       head: [],
       key: 'content',
       body: [
@@ -64,6 +69,7 @@ function getEditTemplate(): SettingOption[] {
     {
       type: 'item',
       name: '<ui-lang>edit.seasons</ui-lang>',
+      icon: 'AllApps',
       head: [],
       key: 'seasons',
       body: [
@@ -81,11 +87,13 @@ function getEditTemplate(): SettingOption[] {
                 type: 'image',
                 key: 'header',
                 data: 'header',
+                classList: ['edit-header'],
               },
               {
                 type: 'group',
                 direction: 'row',
                 key: 'cover_seasonInfo',
+                classList: ['edit-season-info-cover'],
                 items: [
                   {
                     type: 'image',
@@ -96,6 +104,7 @@ function getEditTemplate(): SettingOption[] {
                     type: 'group',
                     key: 'seasonInfo',
                     direction: 'column',
+                    classList: ['edit-season-info'],
                     items: [
                       {
                         type: 'text',
@@ -133,6 +142,36 @@ function getEditTemplate(): SettingOption[] {
                         step: 1,
                         max: Infinity,
                       },
+                      {
+                        type: 'text',
+                        text: '<ui-lang class="edit-link-title">edit.links</ui-lang><ui-lang class="edit-link-name">edit.links_name</ui-lang><ui-lang>edit.links_url</ui-lang>',
+                        key: 'links_text'
+                      },
+                      {
+                        type: 'list',
+                        key: 'links',
+                        data: 'links',
+                        inline: true,
+                        template: {
+                          type: 'group',
+                          direction: 'row',
+                          key: 'value',
+                          items: [
+                            {
+                              type: 'input',
+                              key: 'name',
+                              data: 'name',
+                              classList: ['edit-link-name'],
+                            },
+                            {
+                              type: 'input',
+                              key: 'url',
+                              data: 'url',
+                              classList: ['edit-links-url'],
+                            },
+                          ],
+                        },
+                      }
                     ],
                   },
                 ],
@@ -145,7 +184,7 @@ function getEditTemplate(): SettingOption[] {
   ];
 }
 
-let tabs: Map<string| number, {setting: SettingTemplate, tab: HTMLDivElement, body: HTMLDivElement, tabName: HTMLDivElement}> = new Map();
+let tabs: Map<string | number, {setting: SettingTemplate, tab: HTMLDivElement, body: HTMLDivElement, tabName: HTMLDivElement, new: boolean}> = new Map();
 
 let tabsElement: HTMLDivElement;
 let bodysElement: HTMLDivElement;
@@ -158,31 +197,62 @@ function switchTab(current: {tab: HTMLDivElement, body: HTMLDivElement}) {
   current.body.classList.add('edit-body-current');
 }
 
+function closeTab(id: any) {
+  let target = tabs.get(id);
+  if (!target) return;
+  let nextTab = target.tab.previousSibling || target.tab.nextSibling;
+  if (tabs.size === 1) {
+    createTab();
+  } else if (target.tab.classList.contains('edit-tab-current')) {
+    tabs.forEach((item)=>{
+      if (item.tab !== nextTab) return;
+      switchTab({tab: item.tab, body: item.body});
+    });
+  }
+  tabs.delete(id);
+  (target as any).tab.remove();
+  setTimeout(() => {
+    (target as any).body.remove();
+  }, 100);
+}
+
 function createTab(bangumi?: Bangumi) {
-  console.log('create');
-  
   let tabId: string | number | undefined = bangumi?.id;
   if (tabId === undefined) {
     tabId = Date.now();
   }
   let setting = new SettingTemplate(getEditTemplate(), bangumi);
+  setting.element.classList.add('ui-width2');
+  let control = document.createElement('div');
+  control.classList.add('edit-control', 'ui-width2');
+  let btnConfirm = document.createElement('button');
+  btnConfirm.classList.add('btn-accent');
+  btnConfirm.innerHTML = '<i class="icon icon-Save"></i><ui-lang>edit.save</ui-lang>';
+  let btnReset = document.createElement('button');
+  btnReset.innerHTML = 'TODO: reset button';
+  control.append(btnConfirm, btnReset);
   let tab = document.createElement('div');
   tab.classList.add('edit-tab', 'edit-tab-current');
   tab.innerHTML = `<div class="edit-tab-name">${bangumi ? bangumi.title : '<ui-lang>edit.new</ui-lang>'}</div><div class="edit-tab-close"><div class="icon icon-Clear"></div></div>`;
   let tabName = (tab.querySelector('.edit-tab-name') as HTMLDivElement);
   let body = document.createElement('div');
   body.classList.add('edit-body','edit-body-current');
-  body.append(setting.element);
-  tabs.set(tabId, {setting, tab, tabName, body});
+  body.append(control, setting.element);
+  tabs.set(tabId, {setting, tab, tabName, body, new: !bangumi});
   tabsElement.querySelectorAll('.edit-tab-current').forEach((el)=>el.classList.remove('edit-tab-current'));
   bodysElement.querySelectorAll('.edit-body-current').forEach((el)=>el.classList.remove('edit-body-current'));
   addBtnElement.before(tab);
   bodysElement.append(body);
   let closeBtn = (tab.querySelector('.edit-tab-close') as HTMLDivElement);
-  closeBtn.addEventListener('click',()=>{});
+  closeBtn.addEventListener('click',()=>closeTab(tabId));
   tab.addEventListener('click',(ev)=>{
     if (ev.composedPath().includes(closeBtn)) return;
     switchTab({tab, body});
+  });
+  tab.addEventListener('mousedown',(ev)=>{
+    if (ev.button !== 1) return;
+    ev.preventDefault();
+    closeTab(tabId);
   });
 }
 
@@ -191,12 +261,20 @@ const page: SinglePageOptions = {
   single: true,
   onCreate(element, option) {
     tabsElement = (element.querySelector('.edit-tabs') as HTMLDivElement);
+    tabsElement.addEventListener('wheel',(ev)=>{
+      tabsElement.scrollLeft += ev.deltaY;
+    });
     bodysElement = (element.querySelector('.edit-bodys') as HTMLDivElement);
     addBtnElement = (tabsElement.querySelector('.edit-tab-add') as HTMLDivElement);
+    addBtnElement.addEventListener('click',()=>{
+      createTab();
+      tabsElement.scrollLeft = tabsElement.scrollWidth;
+    });
   },
   onBack(element, option) {
   },
   onOpen(element, option) {
+    if (tabs.size > 0) return;
     createTab();
   },
 };
