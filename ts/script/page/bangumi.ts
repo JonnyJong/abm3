bca2f3440ee05010c6cc51225d85ed3e724dfa99
JonnyJong <jonnyjong@outlook.com>
2023-08-12 09:41:06 +0800

    update: page bangumi evaluation

--- ts/script/page/bangumi.ts
+++ ts/script/page/bangumi.ts
@@ -72,18 +72,55 @@ function initSwitchSeason(element: HTMLElement) {
   element.querySelectorAll('.bangumi-season').forEach((btn, i)=>btn.addEventListener('click', ()=>switchSeason(element, btn, i)));
 }
 
+function initFavoriteButton(element: HTMLElement, id: string, page: Page) {
+  let icon = element.querySelector('.bangumi-favorite .icon') as HTMLDivElement;
+  element.querySelector('.bangumi-favorite')?.addEventListener('click',async ()=>{
+    let before = db.items[id].favorite;
+    await db.items[id].edit({favorite: !before, updated: Date.now()});
+    page.date = db.items[id].updated;
+    if (before) {
+      icon.classList.remove('icon-HeartFill');
+      icon.classList.add('icon-Heart');
+      return;
+    }
+    icon.classList.add('icon-HeartFill');
+    icon.classList.remove('icon-Heart');
+  });
+}
+function initRateButton(element: HTMLElement, id: string, page: Page) {
+  let rate = element.querySelector('.bangumi-rate') as HTMLDivElement;
+  rate.addEventListener('click',async (ev)=>{
+    let level = Array.from(rate.children).indexOf(ev.target as Element) + 1 as 0 | 1 | 2 | 3 | 4 | 5;
+    if (level === 0 || level > 5) return;
+    if (db.items[id].stars === level) {
+      level = 0;
+    }
+    await db.items[id].edit({stars: level, updated: Date.now()});
+    page.date = db.items[id].updated;
+    let html = '';
+    for (let i = 1; i < 6; i++) {
+      html += `<div class="icon icon-FavoriteStar${i <= level ? 'Fill' : ''}"></div>`;
+    }
+    rate.innerHTML = html;
+  });
+}
+
 class page implements PageOptions {
   name = "bangumi";
-  onCreate (element: HTMLElement, option: any) {
+  onCreate (element: HTMLElement, option: any, page: Page) {
     element.addEventListener('scroll',()=>{
       element.style.setProperty('--h', element.scrollTop + 'px');
     });
     initSwitchSeason(element);
+    initFavoriteButton(element, option, page);
+    initRateButton(element, option, page);
   };
   onBack (element: HTMLElement, option: any, page: Page) {
     if (db.items[option]?.updated === page.date) return;
     element.innerHTML = layout('page/bangumi', this.layoutHandler(option, page));
     initSwitchSeason(element);
+    initFavoriteButton(element, option, page);
+    initRateButton(element, option, page);
   };
   onClose(){};
   layoutHandler(option: any, page: Page) {
