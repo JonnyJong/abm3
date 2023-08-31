1b74a69649d802d8fdf7e6bcb01e4a589947edb5
JonnyJong <jonnyjong@outlook.com>
2023-08-31 11:46:44 +0800

    update: build

--- build.js
+++ build.js
@@ -23,17 +23,23 @@ try {
 }
 
 // yaml
-function generateYaml(dir) {
+function generateYaml(dir, createManifest) {
   let configList = readdirSync(dir, { withFileTypes: true });
+  let manifest = [];
   configList.forEach((dirent)=>{
     if (!dirent.isFile()) return;
     if(!['.yml','.yaml'].includes(path.extname(dirent.name))) return;
+    let name = path.parse(dirent.name).name;
+    if (createManifest) manifest.push(name);
     try {
-      writeFileSync(path.join(dir, path.parse(dirent.name).name + '.json'), JSON.stringify(yaml.parse(readFileSync(path.join(dir, dirent.name), 'utf-8'))), 'utf-8');
+      writeFileSync(path.join(dir, name + '.json'), JSON.stringify(yaml.parse(readFileSync(path.join(dir, dirent.name), 'utf-8'))), 'utf-8');
     } catch (error) {
       console.error(error);
     }
   });
+  if (!createManifest) return;
+  manifest.sort();
+  writeFileSync(path.join(dir, 'manifest.json'), JSON.stringify(manifest), 'utf-8');
 }
 generateYaml('./configs');
-generateYaml('./locales');
+generateYaml('./locales', true);
